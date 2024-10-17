b7a26d9530555c459305000845d1424679a5cd8c
t <timofeyivv@gmail.com>
2024-10-17 11:26:28 +1000

    add view files and templates

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('title', 'Авторизация')
+
+@section('header_title', 'Логин')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="card col-md-6">
+            <div class="card-header">
+                <h2>Вход в аккаунт</h2>
+            </div>
+            <div class="card-body">
+                <form action="" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label" for="login">Логин:</label>
+                        <input class="form-control" type="text" id="login" name="login" required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="password">Пароль:</label>
+                        <input class="form-control" type="password" id="password" name="password" required>
+                    </div>
+                    <button class="btn btn-primary" type="submit">
+                        Login
+                    </button>
+                    <a href="" class="btn btn-link">Регистрация</a>
+                    <a href="" class="btn btn-secondary">На главную</a>
+                </form>
+                @if (session('error'))
+                    <div class="alert alert-danger m-3">
+                        {{ session('error') }}
+                    </div>
+                @endif
+
+                @if (session('success'))
+                    <div class="alert alert-success m-3">
+                        {{ session('success') }}
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('title', 'Registration')
+
+@section('header_title', 'Registration')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="card col-md-6">
+            <div class="card-header">
+                <h2>Создание аккаунта</h2>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('user.register') }}" method="POST" onsubmit="return checkPasswords()">
+                    @csrf
+
+                    <div class="mb-3">
+                        <label for="login" class="form-label">Логин:</label>
+                        <input type="text" class="form-control" id="login" name="login" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label" for="password">Пароль:</label>
+                        <input class="form-control" type="password" id="password" name="password" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label" for="confirm_password">Подтверждение пароля:</label>
+                        <input class="form-control" type="password" id="password_confirmation"
+                               name="password_confirmation" required>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">
+                        Register
+                    </button>
+                    <a href="{{ route('login') }}" class="btn btn-link">Login</a>
+                    <a href="{{ route('welcome') }}" class="btn btn-secondary">Back to Home</a>
+                </form>
+                @if ($errors->any())
+                    <div class="alert alert-danger m-3">
+                        <ul class="mb-0">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script src="{{ asset('js/register.js' )}}"
+@endpush
