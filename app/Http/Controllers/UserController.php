6fdd07f9134d0fccad875c11bd12dc68bc94359e
t <timofeyivv@gmail.com>
2024-10-19 19:36:37 +1000

    add: login validation logic, user login method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function register(Request $request): RedirectResponse
+    public function register(RegisterRequest $request): RedirectResponse
     {
         $data = $request->validated();
         $user = $this->createUser($data);
-        
+
+        Auth::login($user);
+        $request->session()->regenerate();
+
+        return redirect()->route('home')->with('success', 'Registration successful');
+    }
+
+    public function login(LoginRequest $request): RedirectResponse
+    {
+        $data = $request->validated();
+
+        //If Authorization success
+        if (Auth::attempt($data)) {
+            $request->session()->regenerate();
+            return redirect()->route('home')->with('success', 'Login successful');
+        }
+
+        return redirect()->route('login')->with('error', 'Invalid login or password');
     }
 
     public function createUser(array $data): User
---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'login' => 'required|string|exists:users,login',
+            'password' => 'required|string|exists:users,password',
+        ];
+    }
+}
