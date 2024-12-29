fbcb3a860e129197fbe5df980a3009dbe4acf9f8
wanderer29 <131830133+wanderer29@users.noreply.github.com>
2024-12-29 19:19:25 +1000

    Add middleware auth and guest

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,17 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('login', [PageController::class, 'showLogin'])->name('login');
-Route::get('register', [PageController::class, 'showRegister'])->name('register');
-Route::get('home', [PageController::class, 'showHome'])->name('home');
-Route::get('/', [PageController::class, 'showWelcome'])->name('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('login', [PageController::class, 'showLogin'])->name('login');
+    Route::get('register', [PageController::class, 'showRegister'])->name('register');
+    Route::get('/', [PageController::class, 'showWelcome'])->name('welcome');
 
+    Route::post('register', [UserController::class, 'register'])->name('register');
+    Route::post('login', [UserController::class, 'login'])->name('login');
+});
 
-Route::post('register', [UserController::class, 'register'])->name('register');
-Route::post('login', [UserController::class, 'login'])->name('login');
-Route::post('logout', [UserController::class, 'logout'])->name('logout');
+Route::middleware('auth')->group(function () {
+    Route::get('home', [PageController::class, 'showHome'])->name('home');
+
+    Route::post('logout', [UserController::class, 'logout'])->name('logout');
+});
