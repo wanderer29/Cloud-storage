fd2f46523dcad0dbab926ef1e41d257c5d91315d
wanderer29 <131830133+wanderer29@users.noreply.github.com>
2024-12-30 12:06:05 +1000

    Add minIO configuration

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,6 +57,16 @@ return [
             'throw' => false,
         ],
 
+        'minio' => [
+            'driver' => 's3',
+            'key' => env('MINIO_ACCESS_KEY', 'admin'),
+            'secret' => env('MINIO_SECRET_KEY', 'password123'),
+            'endpoint' => env('MINIO_ENDPOINT', 'http://localhost:9000'),
+            'region' => env('MINIO_REGION', 'us-east-1'),
+            'bucket' => env('MINIO_BUCKET', 'my-bucket'),
+            'use_path_style_endpoint' => true,
+        ],
+
     ],
 
     /*
